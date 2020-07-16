b2710bc110aff47bef7783a85eb3bead95660235
unknown <tereza.raidlova@gmail.com>
2020-07-16 17:50:03 +0200

    added router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home } from './components/Home';
+import { About } from './components/About';
+import { Contact } from './components/Contact';
+import { NoMatch } from './components/NoMatch';
 
 function App() {
-	return <React.Fragment></React.Fragment>;
+	return (
+		<React.Fragment>
+			<Router>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route path="/about" component={About} />
+					<Route path="/contact" component={Contact} />
+					<Route component={NoMatch} />
+				</Switch>
+			</Router>
+		</React.Fragment>
+	);
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Home = () => (
+	<div>
+		<h2>lol</h2>
+		<p>lorem</p>
+	</div>
+);
